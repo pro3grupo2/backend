fcd7daebe296b8e257f7537a5d18d7163fc740ce
adriantoral <adrian.rubiboy11@gmail.com>
2024-03-03 17:26:47 +0100

    Arregla un error en los validadores de las materias.js

--- source/proyectos3/validators/materias.js
+++ source/proyectos3/validators/materias.js
@@ -1,30 +1,30 @@
 const {body, param} = require('express-validator')
 const {validate} = require('./validation')
 
-const get_premio = [
+const get_materia = [
     param('premio_id', 'Type: Int').exists().toInt(),
     validate
 ]
 
-const create_premio = [
+const create_materia = [
     body('titulo', 'Type: String, Max-Length: 50').exists().notEmpty().isString().isLength({max: 50}),
     validate
 ]
 
-const update_premio = [
+const update_materia = [
     param('premio_id', 'Type: Int').exists().toInt(),
     body('titulo', 'Type: String, Max-Length: 50').exists().notEmpty().isString().isLength({max: 50}).optional(),
     validate
 ]
 
-const delete_premio = [
+const delete_materia = [
     param('premio_id', 'Type: Int').exists().toInt(),
     validate
 ]
 
 module.exports = {
-    get_premio,
-    create_premio,
-    update_premio,
-    delete_premio
+    get_materia,
+    create_materia,
+    update_materia,
+    delete_materia
 }

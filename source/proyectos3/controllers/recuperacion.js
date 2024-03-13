9f13180857968e1a6ede21d1eb545a29953f8f2a
adriantoral <adrian.rubiboy11@gmail.com>
2024-03-13 16:27:54 +0100

    Realiza el merge con el redis #7 Arregla un error con el import del ORM de prisma

--- source/proyectos3/controllers/recuperacion.js
+++ source/proyectos3/controllers/recuperacion.js
@@ -3,10 +3,7 @@ const correoHtml = require('../mailes/correo');
 const recuperacion_errors = require("../errors/recuperacion")
 
 const enviarCorreo = async (req, res) => {
-    const {MATCHED} = req
-    //console.log(req.body)
-    //console.log(req.body.correo)
-    const data = await recuperacion_service.obtenerUsuario(req.body.correo)
+    const data = await recuperacion_service.obtenerUsuario(req.MATCHED.correo)
 
     if (!data) return res.status(400).send({
         data: {
@@ -20,29 +17,20 @@ const enviarCorreo = async (req, res) => {
         .replace('{{nombre_completo}}', data.nombre_completo)
         .replace(/{{to_link}}/g, "https://www.google.com");
 
-    var templateParams = {
+    const templateParams = {
         subject: "Recuperacion contraseña repositorio utad",
         to_email: data.correo,
         message: mensaje
     };
-    
-    const resultado = await recuperacion_service.enviarCorreo(templateParams)
-    //console.log("resultado: " + resultado)
-    if (resultado==1){
-        //console.log("ta wapo")
-        return res.send({
-            data: data.correo
-        });
-    }
 
-    else{
-        return res.status(400).send({
-            data: {
-                errors: [recuperacion_errors.WRONG_SEND]
-            }
-        });
-    }
-    
+    if (!!await recuperacion_service.enviarCorreo(templateParams)) return res.send({
+        data: data.correo
+    })
+    else return res.status(400).send({
+        data: {
+            errors: [recuperacion_errors.WRONG_SEND]
+        }
+    })
 }
 
 module.exports = {enviarCorreo};
--- source/proyectos3/services/recuperacion.js
+++ source/proyectos3/services/recuperacion.js
@@ -1,6 +1,4 @@
-
-const {PrismaClient} = require('@prisma/client')
-const prisma = new PrismaClient()
+const prisma = require('../databases/mysql')
 const nodemailer = require('nodemailer');
 
 const obtenerUsuario = async (correo) => {

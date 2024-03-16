9361497ad93c082d6a28216de220302ee154b861
adriantoral <adrian.rubiboy11@gmail.com>
2024-03-16 16:53:56 +0100

    Optimiza las llamadas a Redis

--- source/proyectos3/services/auth.js
+++ source/proyectos3/services/auth.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken')
 const bcryptjs = require("bcryptjs")
 const prisma = require('../databases/mysql')
-const redis = require('../databases/redis')
+const {exists, escribir_cache, limpiar_cache, leer_cache} = require('../databases/redis')
 const auth_errors = require("../errors/auth")
 const {hook_updates} = require("../databases/discord")
 
@@ -16,28 +16,30 @@ const verificar_JWT = (token) => {
 }
 
 const get_data_by_correo = async (correo) => {
-    let data
-    if (await redis.exists(`cached:${correo}`)) data = JSON.parse(await redis.get(`cached:${correo}`))
-    else {
-        data = await prisma.usuarios.findUnique({
-            where: {
-                correo: correo
-            }, include: {
-                proyectos: {
-                    select: {
-                        id: true, titulo: true, portada: true
-                    }
+    let data = await leer_cache(`cached:${correo}`)
+    if (data) return data
+
+    data = await prisma.usuarios.findUnique({
+        where: {
+            correo: correo
+        }, include: {
+            proyectos: {
+                select: {
+                    id: true, titulo: true, portada: true
                 }
             }
-        })
-
-        if (data) {
-            await redis.set(`cached:${correo}`, JSON.stringify(data))
-            await redis.expire(`cached:${correo}`, process.env.REDIS_SIGNIN_EXPIRES_IN)
-
-            await hook_updates.success("Nueva sesion iniciada", new Date().toISOString(), JSON.stringify(data))
         }
-    }
+    })
+
+    if (!data) return null
+
+    await escribir_cache([
+        {
+            key: `cached:${correo}`,
+            data: data
+        }
+    ], process.env.REDIS_SIGNIN_EXPIRES_IN)
+    await hook_updates.success("Nueva sesion iniciada", new Date().toISOString(), JSON.stringify(data))
 
     return data
 }
@@ -46,7 +48,7 @@ const signin = async (correo, password) => {
     const data = await get_data_by_correo(correo)
 
     if (!data) {
-        if (await redis.exists(`pending:${correo}`))
+        if (await exists(`pending:${correo}`))
             throw new Error(auth_errors.PENDING_SIGNUP)
 
         throw new Error(auth_errors.WRONG_MAIL)
@@ -61,16 +63,19 @@ const signin = async (correo, password) => {
 const signup_cache = async (usuario) => {
     const key = `pending:${usuario.correo}`
 
-    if (await redis.exists(key))
+    if (await exists(key))
         throw new Error(`${auth_errors.PENDING_SIGNUP} : ${usuario.correo}`)
 
     if (await get_data_by_correo(usuario.correo))
         throw new Error(`${auth_errors.ALREADY_SIGNUP} : ${usuario.correo}`)
 
     try {
-        await redis.set(key, JSON.stringify(usuario))
-        await redis.expire(key, process.env.REDIS_SIGNUP_EXPIRES_IN)
-
+        await escribir_cache([
+            {
+                key: key,
+                data: usuario
+            }
+        ], process.env.REDIS_SIGNUP_EXPIRES_IN)
         await hook_updates.success("Nuevo usuario pendiente de registro", new Date().toISOString(), JSON.stringify(usuario))
 
         return jwt.sign({cache_key: key}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_SIGNUP_EXPIRES_IN})
@@ -80,16 +85,14 @@ const signup_cache = async (usuario) => {
 }
 
 const signup_validate = async (cache_key) => {
-    if (!await redis.exists(cache_key))
+    if (!await exists(cache_key))
         throw new Error(`${auth_errors.INVALID_SIGNUP_TOKEN} : ${cache_key}`)
 
     try {
-        const data = JSON.parse(await redis.get(cache_key))
-        await redis.del(cache_key)
-
+        const data = await leer_cache(cache_key)
+        await limpiar_cache([cache_key])
+        await signup(data)
         await hook_updates.success("Nuevo usuario registrado", new Date().toISOString(), JSON.stringify(data))
-
-        return await signup(data)
     } catch (e) {
         throw new Error(`${auth_errors.WRONG_SIGNUP} : ${cache_key} : ${e.message}`)
     }
@@ -97,16 +100,10 @@ const signup_validate = async (cache_key) => {
 
 const signup = async (usuario) => {
     try {
+        usuario.password = bcryptjs.hashSync(usuario.password)
         return await prisma.usuarios.create({
-            data: {
-                alias: usuario.alias,
-                correo: usuario.correo,
-                nombre_completo: usuario.nombre_completo,
-                password: bcryptjs.hashSync(usuario.password),
-                frase_recuperacion: usuario.frase_recuperacion,
-                rol: usuario.rol,
-                promocion: usuario.promocion
-            }, select: {
+            data: usuario,
+            select: {
                 id: true, correo: true, nombre_completo: true, alias: true, rol: true, promocion: true
             }
         })
--- source/proyectos3/services/premios.js
+++ source/proyectos3/services/premios.js
@@ -1,38 +1,11 @@
 // Dependencias necesarias para la interacción con la base de datos y la creación de tokens
 const prisma = require('../databases/mysql')
-const redis = require('../databases/redis')
+const {escribir_cache, leer_cache, limpiar_cache} = require('../databases/redis')
 const {hook_updates} = require('../databases/discord')
 const premios_errors = require('../errors/premios')
 
-const guardar_cache = async (data, id = null) => {
-    if (id) {
-        await redis.set(`cached:premios:${id}`, JSON.stringify(data))
-        await redis.expire(`cached:premios:${id}`, process.env.REDIS_SIGNIN_EXPIRES_IN)
-    } else {
-        await redis.set('cached:premios', JSON.stringify(data))
-        await redis.expire('cached:premios', process.env.REDIS_SIGNIN_EXPIRES_IN)
-    }
-}
-
-const leer_cache = async (id = null) => {
-    if (id && await redis.exists(`cached:premios:${id}`))
-        return JSON.parse(await redis.get(`cached:premios:${id}`))
-    else if (await redis.exists('cached:premios'))
-        return JSON.parse(await redis.get('cached:premios'))
-    else
-        return null
-}
-
-const limpiar_cache = async (id = null) => {
-    if (await redis.exists('cached:premios'))
-        await redis.del('cached:premios')
-
-    if (id && await redis.exists(`cached:premios:${id}`))
-        await redis.del(`cached:premios:${id}`)
-}
-
 const get_premios = async (skip = 0, take = 20) => {
-    let data = leer_cache()
+    let data = await leer_cache('cached:premios')
     if (data) return data
 
     data = await prisma.premios.findMany({
@@ -42,14 +15,19 @@ const get_premios = async (skip = 0, take = 20) => {
     if (!data)
         throw new Error(premios_errors.IS_EMPTY)
 
-    await guardar_cache(data)
+    await escribir_cache([
+        {
+            key: 'cached:premios',
+            data: data
+        }
+    ], process.env.REDIS_SIGNIN_EXPIRES_IN)
     await hook_updates.success("Premios cacheados", new Date().toISOString(), JSON.stringify(data))
 
     return data
 }
 
 const get_premio = async (id) => {
-    let data = leer_cache(id)
+    let data = await leer_cache(`cached:premios:${id}`)
     if (data) return data
 
     data = await prisma.premios.findUnique({
@@ -61,7 +39,12 @@ const get_premio = async (id) => {
     if (!data)
         throw new Error(premios_errors.NOT_FOUND)
 
-    await guardar_cache(data, id)
+    await escribir_cache([
+        {
+            key: `cached:premios:${id}`,
+            data: data
+        }
+    ], process.env.REDIS_SIGNIN_EXPIRES_IN)
     await hook_updates.success("Premio cacheado", new Date().toISOString(), JSON.stringify(data))
 
     return data
@@ -69,13 +52,20 @@ const get_premio = async (id) => {
 
 const create_premio = async (premio) => {
     try {
-        await limpiar_cache()
+        await limpiar_cache([
+            'cached:premios'
+        ])
 
         const data = await prisma.premios.create({
             data: premio
         })
 
-        await guardar_cache(data, data.id)
+        await escribir_cache([
+            {
+                key: `cached:premios:${data.id}`,
+                data: data
+            }
+        ], process.env.REDIS_SIGNIN_EXPIRES_IN)
         await hook_updates.success("Premio creado", new Date().toISOString(), JSON.stringify(data))
 
         return data
@@ -87,7 +77,10 @@ const create_premio = async (premio) => {
 
 const update_premio = async (id, premio_nuevo) => {
     try {
-        await limpiar_cache(id)
+        await limpiar_cache([
+            'cached:premios',
+            `cached:premios:${id}`
+        ])
 
         const data = await prisma.premios.update({
             where: {
@@ -95,7 +88,12 @@ const update_premio = async (id, premio_nuevo) => {
             }, data: premio_nuevo
         })
 
-        await guardar_cache(data, id)
+        await escribir_cache([
+            {
+                key: `cached:premios:${data.id}`,
+                data: data
+            }
+        ], process.env.REDIS_SIGNIN_EXPIRES_IN)
         await hook_updates.success("Premio actualizado", new Date().toISOString(), JSON.stringify(data))
 
         return data
@@ -106,7 +104,10 @@ const update_premio = async (id, premio_nuevo) => {
 
 const delete_premio = async (id) => {
     try {
-        await limpiar_cache(id)
+        await limpiar_cache([
+            'cached:premios',
+            `cached:premios:${id}`
+        ])
 
         const data = await prisma.premios.delete({
             where: {
@@ -114,7 +115,6 @@ const delete_premio = async (id) => {
             }
         })
 
-        await guardar_cache(data, id)
         await hook_updates.success("Premio eliminado", new Date().toISOString(), JSON.stringify(data))
 
         return data
